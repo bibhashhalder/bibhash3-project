bb06c36fc4af5d01e9d6ef10cce24158801b3643
bibhash halder <halderbibhash80@gmail.com>
2023-05-09 14:32:08 +0600

    third commit added

--- src/component/MainBodyFour/MainBodyFour.jsx
+++ src/component/MainBodyFour/MainBodyFour.jsx
@@ -1,3 +1,29 @@
+import image from "../../../public/image/bd2.png";
+import image1 from "../../../public/image/why.png";
+import "./MainBodyFour.scss";
 export default function MainBodyFour() {
-  return <div>MainBodyFour</div>;
+  return (
+    <div className="mainCoutainerFour">
+      <div className="article">
+        <div className="articleContainer">
+          <div className="imageContainer">
+            <img className="image" src={image} alt="" />
+          </div>
+          <div className="text">
+            <h1 className="headingOne">why quaerere veritatem?</h1>
+            <p className="pragraph">
+              Do you have a documentary brewing and need a place to publish it?
+              At QV we are always in pursuit of new and exciting stories, so if
+              you have an unpublished documentary contact us and let us see if
+              we can work together.
+            </p>
+            <button className="button">contact us</button>
+          </div>
+        </div>
+        <div className="articleContainer1">
+          <img className="image1" src={image1} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
